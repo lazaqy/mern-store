fec2e28773ad29bed8be8a7194e66576a0764d0e
Dian Lassaki <lazaqy@gmail.com>
2025-03-27 20:14:24 +0700

    update auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,34 +1,37 @@
 import jwt from "jsonwebtoken";
 
-const auth = (req, res, next) => {
-    try {
-        const token = req.cookies.accessToken || req.headers?.authorization?.split(" ")[1];
-        if (!token) {
-            return res.status(401).json({
-              message: "No token provided",
-              success: false,
-              error: true,
-            });
-        }
-        const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
-        if (!decoded) {
-            return res.status(401).json({
-                message: "Unauthorized",
-                success: false,
-                error: true,
-            });
-        }
-        req.userId = decoded.id;
-        
-        next();
+const auth = async (request, response, next) => {
+  try {
+    const token =
+      request.cookies.accessToken ||
+      request?.headers?.authorization?.split(" ")[1];
 
-    } catch (error) {
-        res.status(500).json({
-            message: "You have not logged in",
-            success: false,
-            error: true,
-        });
+    if (!token) {
+      return response.status(401).json({
+        message: "Provide token",
+      });
     }
+
+    const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+
+    if (!decode) {
+      return response.status(401).json({
+        message: "Unauthorized Access",
+        error: true,
+        success: false,
+      });
+    }
+
+    request.userId = decode.id;
+
+    next();
+  } catch (error) {
+    return response.status(500).json({
+      message: "You have not login",
+      error: true,
+      success: false,
+    });
+  }
 };
 
 export default auth;
